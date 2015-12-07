5414e56bed80ca18928ddecd9c0b06919c9c2d4f
Konstantin A. Zolotukhin <masted@gmail.com>
2015-12-07 19:28:45 +0200

    Task#properties and Operation#properties index mapping removed, now it is assumed to be indexed as custom properties.

--- ametiste-routine-core-spring/src/main/java/org/ametiste/routine/infrastructure/persistency/jpa/data/OperationPropertyData.java
+++ ametiste-routine-core-spring/src/main/java/org/ametiste/routine/infrastructure/persistency/jpa/data/OperationPropertyData.java
@@ -4,9 +4,7 @@ import javax.persistence.*;
 
 @Entity
 @Table(name = "ame_routine_task_operation_property", indexes = {
-        @Index(name = "op_prop_op_id_idx", columnList = "operation_id", unique = false),
-        @Index(name = "op_prop_name_idx", columnList = "name", unique = false),
-        @Index(name = "op_prop_value_idx", columnList = "value", unique = false)
+        @Index(name = "op_prop_op_id_idx", columnList = "operation_id", unique = false)
 })
 public class OperationPropertyData {
 
--- ametiste-routine-core-spring/src/main/java/org/ametiste/routine/infrastructure/persistency/jpa/data/TaskPropertyData.java
+++ ametiste-routine-core-spring/src/main/java/org/ametiste/routine/infrastructure/persistency/jpa/data/TaskPropertyData.java
@@ -4,9 +4,7 @@ import javax.persistence.*;
 
 @Entity
 @Table(name = "ame_routine_task_property", indexes = {
-        @Index(name = "task_prop_task_id_idx", columnList = "task_id", unique = false),
-        @Index(name = "task_prop_name_idx", columnList = "name", unique = false),
-        @Index(name = "task_prop_value_idx", columnList = "value", unique = false)
+        @Index(name = "task_prop_task_id_idx", columnList = "task_id", unique = false)
 })
 public class TaskPropertyData {
 
