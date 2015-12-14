fe913c55ca2d20616f299ba63eb16b4489b33cfc
Konstantin A. Zolotukhin <masted@gmail.com>
2015-12-14 11:49:09 +0200

    hm

---
+++ ametiste-routine-core-spring/src/generated/java/org/ametiste/routine/infrastructure/persistency/jpa/data/OperationPropertyData_.java
@@ -0,0 +1,16 @@
+package org.ametiste.routine.infrastructure.persistency.jpa.data;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value = "org.hibernate.jpamodelgen.JPAMetaModelEntityProcessor")
+@StaticMetamodel(OperationPropertyData.class)
+public abstract class OperationPropertyData_ {
+
+	public static volatile SingularAttribute<OperationPropertyData, String> name;
+	public static volatile SingularAttribute<OperationPropertyData, Integer> id;
+	public static volatile SingularAttribute<OperationPropertyData, String> value;
+
+}
+
