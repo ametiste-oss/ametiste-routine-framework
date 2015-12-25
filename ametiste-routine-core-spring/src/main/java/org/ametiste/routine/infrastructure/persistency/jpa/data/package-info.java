aca7527cb7aeb981b16b131bf2d76ebda963e961
abliznyuk <bliznyuk.andrey@gmail.com>
2015-12-25 23:28:14 +0200

    fix javadoc

--- ametiste-routine-core-spring/src/main/java/org/ametiste/routine/infrastructure/persistency/jpa/data/package-info.java
+++ ametiste-routine-core-spring/src/main/java/org/ametiste/routine/infrastructure/persistency/jpa/data/package-info.java
@@ -1,14 +1,14 @@
 /**
- * This package conntains {@code Routine Core} data model definition used with JPA repository implementation.
+ * This package contains {@code Routine Core} data model definition used with JPA repository implementation.
  *
- * There is standard JPA model, compatible with {@code Hibernate} 4.3>.
+ * There is standard JPA model, compatible with {@code Hibernate} 4.3 or later.
  *
  * Note, to support {@link SpringDataTaskRepository#deleteTasks(java.util.List, java.time.Instant)} operation,
- * schema for this model must contain set of {@code ON DELETE CASCADE} constaints. With the reference
- * implementation this constraint intorduced via update ddl. See update scripts located in
+ * schema for this model must contain set of {@code ON DELETE CASCADE} constraints. With the reference
+ * implementation this constraint introduced via update ddl. See update scripts located in
  * {@code classpath://update.sql} file for implementation details.
- *
- * @see update.sql
+ * <p>
+ * See {@code update.sql} in module classpath.
  */
 package org.ametiste.routine.infrastructure.persistency.jpa.data;
 import org.ametiste.routine.infrastructure.persistency.jpa.SpringDataTaskRepository;
