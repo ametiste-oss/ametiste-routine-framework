b6a4f5b7df829816e295161e7082c176c1c63e82
abliznyuk <bliznyuk.andrey@gmail.com>
2016-04-07 10:45:55 +0300

    generated sources with gradle plugin

---
+++ ametiste-routine-core-spring/src/generated/java/org/ametiste/routine/infrastructure/persistency/jpa/data/TaskData_.java
@@ -0,0 +1,26 @@
+package org.ametiste.routine.infrastructure.persistency.jpa.data;
+
+import java.util.Date;
+import java.util.UUID;
+import javax.annotation.Generated;
+import javax.persistence.metamodel.ListAttribute;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value = "org.hibernate.jpamodelgen.JPAMetaModelEntityProcessor")
+@StaticMetamodel(TaskData.class)
+public abstract class TaskData_ {
+
+	public static volatile ListAttribute<TaskData, TaskNoticeData> notices;
+	public static volatile SingularAttribute<TaskData, Date> completionTime;
+	public static volatile ListAttribute<TaskData, OperationData> operations;
+	public static volatile SingularAttribute<TaskData, Date> creationTime;
+	public static volatile SingularAttribute<TaskData, String> creatorId;
+	public static volatile SingularAttribute<TaskData, Date> executionStartTime;
+	public static volatile SingularAttribute<TaskData, String> schemeId;
+	public static volatile SingularAttribute<TaskData, UUID> id;
+	public static volatile SingularAttribute<TaskData, String> state;
+	public static volatile ListAttribute<TaskData, TaskPropertyData> properties;
+
+}
+
---
+++ ametiste-routine-core-spring/src/generated/java/org/ametiste/routine/infrastructure/persistency/jpa/data/TaskNoticeData_.java
@@ -0,0 +1,16 @@
+package org.ametiste.routine.infrastructure.persistency.jpa.data;
+
+import java.time.Instant;
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value = "org.hibernate.jpamodelgen.JPAMetaModelEntityProcessor")
+@StaticMetamodel(TaskNoticeData.class)
+public abstract class TaskNoticeData_ {
+
+	public static volatile SingularAttribute<TaskNoticeData, Instant> creationTime;
+	public static volatile SingularAttribute<TaskNoticeData, String> text;
+
+}
+
