0540e08d830a8b25973346359a8d5bec40ec792c
Konstantin A. Zolotukhin <masted@gmail.com>
2016-04-14 14:40:22 +0300

    cleanup

--- ametiste-routine-core/src/main/java/org/ametiste/routine/infrastructure/protocol/moddata/DataConverter.java
+++ ametiste-routine-core/src/main/java/org/ametiste/routine/infrastructure/protocol/moddata/DataConverter.java
@@ -1,13 +1,21 @@
 package org.ametiste.routine.infrastructure.protocol.moddata;
 
-import static javafx.scene.input.KeyCode.T;
-
 /**
+ * Simple data conversion interface, used as plugin interface to adopt platform-specific
+ * solutions for types convertion.
  *
- * @since
+ * @since 1.1
  */
 public interface DataConverter {
 
+    /**
+     * Just converst the given value to the object of the specified type.
+     *
+     * @param value source value, can be null.
+     * @param type  class of target type
+     * @param <T> target type
+     * @return object of the target type, can be null
+     */
     <T> T convert(Object value, Class<T> type);
 
 }
