59c2a9accdbd3a96f022bc6afd6606f7524ee938
Konstantin A. Zolotukhin <masted@gmail.com>
2016-04-14 19:18:31 +0300

    BacklogRenewOperationScheme name fix. Now it has client metrics.

--- ametiste-routine-mod-backlog/src/main/java/org/ametiste/routine/mod/backlog/application/operation/BacklogRenewOperationScheme.java
+++ ametiste-routine-mod-backlog/src/main/java/org/ametiste/routine/mod/backlog/application/operation/BacklogRenewOperationScheme.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Component;
 @Component
 public class BacklogRenewOperationScheme extends StatelessOperationScheme<BacklogParams> {
 
-    public static final String NAME = ModBacklog.MOD_ID + "::renewOperation";
+    public static final String NAME = ModBacklog.MOD_ID + "-renew-operation";
 
     public BacklogRenewOperationScheme() {
         super(NAME, DirectBacklogParams::new, BacklogRenewExecutor::new);
